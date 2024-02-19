da23afc0ab6561983cea41925baf6f2980f6d627
Goofyz <129243293+GoofyzHealth@users.noreply.github.com>
2024-02-19 12:42:01 +0700

    Update pe_web_app.py

--- pe_web_app.py
+++ pe_web_app.py
@@ -10,7 +10,6 @@ import pandas as pd
 import numpy as np
 import calendar
 import streamlit as st
-import base64
 import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime
