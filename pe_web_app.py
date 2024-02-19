f84a9fe7a43ab893fb0bc2b96be636f9a32b5ccd
Goofyz <129243293+GoofyzHealth@users.noreply.github.com>
2024-02-19 13:07:08 +0700

    Update pe_web_app.py

--- pe_web_app.py
+++ pe_web_app.py
@@ -10,12 +10,12 @@ import pandas as pd
 import numpy as np
 import calendar
 import base64
+from st_on_hover_tabs import on_hover_tabs
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime
 from streamlit_gsheets import GSheetsConnection
-from st_on_hover_tabs import on_hover_tabs
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier 
 st.set_page_config(layout="wide")
