fd7f00129a9efa08c05e83036edbe99bb22ee155
Goofyz <129243293+GoofyzHealth@users.noreply.github.com>
2024-02-19 17:43:28 +0700

    Update pe_web_app.py

--- pe_web_app.py
+++ pe_web_app.py
@@ -10,7 +10,6 @@ import pandas as pd
 import numpy as np
 import calendar
 import base64
-import st_on_hover_tabs
 from st_on_hover_tabs import on_hover_tabs
 import streamlit as st
 import plotly.express as px
